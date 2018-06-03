2f5f32e3822ef296f26197d35cd1e4001378e430
Reed Jones <reed.jones@pderas.com>
2018-06-02 21:39:58 -0600

    removed commented line

--- src/helpers.php
+++ src/helpers.php
@@ -10,6 +10,5 @@ if ( ! function_exists('config_path'))
     function config_path($path = '')
     {
         return app()->basePath() . '/config' . ($path ? '/' . $path : $path);
-        // return app()->make('path.config').($path ? DIRECTORY_SEPARATOR.$path : $path);
     }
 }
