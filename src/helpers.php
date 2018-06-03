8cd2ff5aedd88e0cd8ac6e0b0686e6bfbf2b374e
Reed Jones <reed.jones@pderas.com>
2018-06-02 20:40:36 -0600

    added helper function

---
+++ src/helpers.php
@@ -0,0 +1,15 @@
+<?php
+if ( ! function_exists('config_path'))
+{
+    /**
+     * Get the configuration path.
+     *
+     * @param  string $path
+     * @return string
+     */
+    function config_path($path = '')
+    {
+        return app()->basePath() . '/config' . ($path ? '/' . $path : $path);
+        // return app()->make('path.config').($path ? DIRECTORY_SEPARATOR.$path : $path);
+    }
+}
