9b736a2212818542c00000459209208dcaae8212
MattApril <Matthew.JApril@gmail.com>
2022-03-25 12:03:10 -0400

    PHP 7.1 minimum version, Laravel 9 support

--- tests/LumenPassport/integration.php
+++ tests/LumenPassport/integration.php
@@ -2,32 +2,46 @@
 
 namespace Dusterio\LumenPassport\Tests;
 
-use Dusterio\LumenPassport\Http\Controllers\AccessTokenController;
 use Dusterio\LumenPassport\LumenPassport;
 use Carbon\Carbon;
 use Laravel\Passport\Passport;
-use Laravel\Passport\PassportServiceProvider;
+use PHPUnit\Framework\TestCase;
 
 /**
  * Class IntegrationTest
  * @package Dusterio\LumenPassport\Tests
  */
-class IntegrationTest extends \PHPUnit_Framework_TestCase
+class IntegrationTest extends TestCase
 {
+
     /**
      * @test
      */
-    public function token_ttl_can_be_set_via_lumen_class()
-    {
-        // Default (global) client
-        LumenPassport::tokensExpireIn(Carbon::now()->addYears(1));
-        $this->assertTrue(Passport::tokensExpireIn() == Carbon::now()->diff(Carbon::now()->addYears(1)));
-        $this->assertTrue(LumenPassport::tokensExpireIn() == Carbon::now()->diff(Carbon::now()->addYears(1)));
+    public function global_token_ttl_can_be_set_via_lumen_class() {
+        $now = Carbon::now();
+        Carbon::setTestNow($now);
+        $expiryDate = $now->clone()->addYear();
+        LumenPassport::tokensExpireIn($expiryDate);
+        $this->assertEquals(Passport::tokensExpireIn(), Carbon::now()->diff($expiryDate));
+        $this->assertEquals(LumenPassport::tokensExpireIn(), Carbon::now()->diff($expiryDate));
+    }
 
-        // Specific client
-        LumenPassport::tokensExpireIn(Carbon::now()->addYears(5), 2);
-        $this->assertTrue(LumenPassport::tokensExpireIn(null, 2) == Carbon::now()->diff(Carbon::now()->addYears(5)));
-        $this->assertTrue(LumenPassport::tokensExpireIn() == Carbon::now()->diff(Carbon::now()->addYears(1)));
-        $this->assertTrue(Passport::tokensExpireIn() == Carbon::now()->diff(Carbon::now()->addYears(1)));
+    /**
+     * @test
+     */
+    public function client_specific_token_ttl_can_be_set_via_lumen_class()
+    {
+        $clientId = 2;
+        $now = Carbon::now();
+        Carbon::setTestNow($now);
+        $clientExpiryDate = $now->clone()->addYears(5);
+        $defaultGlobalExpiryDate = $now->clone()->addYears(1);
+
+        LumenPassport::tokensExpireIn($clientExpiryDate, $clientId);
+        $this->assertEquals(LumenPassport::tokensExpireIn(null, $clientId), Carbon::now()->diff($clientExpiryDate));
+
+        # global TTL should still default to 1 year
+        $this->assertEquals(LumenPassport::tokensExpireIn(), Carbon::now()->diff($defaultGlobalExpiryDate));
+        $this->assertEquals(Passport::tokensExpireIn(), Carbon::now()->diff($defaultGlobalExpiryDate));
     }
 }
